5ae34faf788affef0b35942e4fd2664d1cf4d744
shani fedida <shani.fedida@gmail.com>
2019-01-06 07:57:25 +0200

    replace movieDB with movieRepo comment out all api calls beside /:movieId in order not implment unneeded functionality in MoviesRepo

--- server/routes/movie.js
+++ server/routes/movie.js
@@ -1,13 +1,12 @@
 const Joi = require('joi');
-const movieDB = require('../models/movie');
 const validationSchema = require('../schemas/validation');
 const express = require('express');
 const router = express.Router();
 const movieRepo = require('../models/movieRepo');
 
-router.get('/', (req, res) => {
-    res.send(movieDB.movies);
-});
+// router.get('/', (req, res) => {
+//     res.send(movieDB.movies);
+// });
 
 router.get('/:movieId', async (req, res) => {
     try {
@@ -23,40 +22,40 @@ router.get('/:movieId', async (req, res) => {
     }
 });
 
-router.post('/', async (req, res) => {
-    const { error } = validateMovieName(req.body.name);
-    if (error) {
-        return res.status(400).send(error.details[0].message);
-    }
-    const newMovie = await movieDB.addNewMovie({
-        name: req.body.name
-    });
-    res.send(newMovie);
-});
+// router.post('/', async (req, res) => {
+//     const { error } = validateMovieName(req.body.name);
+//     if (error) {
+//         return res.status(400).send(error.details[0].message);
+//     }
+//     const newMovie = await movieDB.addNewMovie({
+//         name: req.body.name
+//     });
+//     res.send(newMovie);
+// });
 
-router.put('/:movieId', async (req, res) => {
-    const movie = await movieDB.getMovieById(req.params.movieId);
-    if (!movie) return res.status(404).send(validationSchema.movieNotFound);
+// router.put('/:movieId', async (req, res) => {
+//     const movie = await movieDB.getMovieById(req.params.movieId);
+//     if (!movie) return res.status(404).send(validationSchema.movieNotFound);
 
-    const { error } = validateMovieName(req.body.name);
-    if (error) {
-        return res.status(400).send(error.details[0].message);
-    }
+//     const { error } = validateMovieName(req.body.name);
+//     if (error) {
+//         return res.status(400).send(error.details[0].message);
+//     }
 
-    await movieDB.updateMovieName(movie, req.body.name);
-    res.send(movie);
-});
+//     await movieDB.updateMovieName(movie, req.body.name);
+//     res.send(movie);
+// });
 
-router.delete('/:movieId', async (req, res) => {
-    const movie = await movieDB.getMovieById(req.params.movieId);
-    if (!movie) return res.status(404).send(validationSchema.movieNotFound);
+// router.delete('/:movieId', async (req, res) => {
+//     const movie = await movieDB.getMovieById(req.params.movieId);
+//     if (!movie) return res.status(404).send(validationSchema.movieNotFound);
 
-    await movieDB.deleteMovie(movie);
-    res.send(movie);
-});
+//     await movieDB.deleteMovie(movie);
+//     res.send(movie);
+// });
 
-function validateMovieName(name) {
-    return Joi.validate(name, validationSchema.movieName);
-}
+// function validateMovieName(name) {
+//     return Joi.validate(name, validationSchema.movieName);
+// }
 
 module.exports = router;
