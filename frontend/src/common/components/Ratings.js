6e92effda518abb3b75dc479ba04e50a00f43253
shani fedida <shani.fedida@gmail.com>
2019-01-08 15:32:48 +0200

    create ratings component with hard coded data in state

---
+++ frontend/src/common/components/Ratings.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import FeatureRating from './Feature-Rating';
+
+class Ratings extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ratings: [
+                { feature: 'funny', rating: 4, maxRating: 5 },
+                { feature: 'interesting', rating: 3, maxRating: 5 },
+                { feature: 'female lead', rating: 3, maxRating: 5 },
+                { feature: 'happy ending', rating: 3, maxRating: 5 },
+                { feature: 'make you think', rating: 3, maxRating: 5 }
+            ]
+        };
+    }
+    render() {
+        return (
+            <div>
+                {this.state.ratings.map(rating => (
+                    <FeatureRating
+                        key={rating.feature}
+                        feature={rating.feature}
+                        rating={rating.rating}
+                        maxRating={rating.maxRating}
+                    />
+                ))}
+            </div>
+        );
+    }
+}
+
+export default Ratings;
