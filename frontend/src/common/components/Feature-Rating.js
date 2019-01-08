73d101c7c4fe9d08e4a6bebba90d34b8eea59e6f
shani fedida <shani.fedida@gmail.com>
2019-01-08 15:16:13 +0200

    format  props

--- frontend/src/common/components/Feature-Rating.js
+++ frontend/src/common/components/Feature-Rating.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import { capitalizeFirstLetter } from '../utils/capitalizeFirstLetter';
 
 class FeatureRating extends Component {
     render() {
         return (
             <div>
-                <span>{this.props.feature}</span>
+                <span>{capitalizeFirstLetter(this.props.feature)}</span>
                 <span> </span>
-                <span>{this.props.rating}</span>
+                <span>{this.props.rating.toFixed(1)}</span>
                 <span>/</span>
                 <span>{this.props.maxRating}</span>
             </div>
