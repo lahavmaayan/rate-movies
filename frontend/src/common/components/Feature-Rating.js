6a585016ccc5cfeb9a82c7598ebeb9f249020f1d
shani fedida <shani.fedida@gmail.com>
2019-01-08 14:41:46 +0200

    create component with static data

---
+++ frontend/src/common/components/Feature-Rating.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class FeatureRating extends Component {
+    render() {
+        return (
+            <div>
+                <span>Funny</span>
+                <span> </span>
+                <span>4.5</span>
+                <span>/</span>
+                <span>5</span>
+            </div>
+        );
+    }
+}
+export default FeatureRating;
