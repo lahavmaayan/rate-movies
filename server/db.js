cd95bd81fc6b7ca1e813a60ae4c2f345cfe54167
Yarin Dekel <yarind@yarind01-mac.local>
2019-01-09 13:40:09 +0200

    removed server warnings
    Signed-off-by: Yarin Dekel <yarind@yarind01-mac.local>

--- server/db.js
+++ server/db.js
@@ -2,16 +2,17 @@ const mongoose = require('mongoose');
 
 module.exports = function() {
     mongoose
-        .connect('mongodb://localhost/rate-movies')
+        .connect(
+            'mongodb://localhost/rate-movies',
+            { useCreateIndex: true, useNewUrlParser: true }
+        )
         .then(() => console.log('connected to mongo db'))
         .catch(err => console.error('could not connect to mongo db', err));
 
     process.on('SIGINT', function() {
         mongoose.connection.close(function() {
             console.log(
-                termination(
-                    'Mongoose default connection is disconnected due to application termination'
-                )
+                'Mongoose default connection is disconnected due to application termination'
             );
             process.exit(0);
         });
