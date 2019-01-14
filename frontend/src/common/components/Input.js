2253528ed271f4845ccbaefab3b85d38933964cc
Iahel Ramos <iahel.ra@gigya-inc.com>
2019-01-14 11:53:10 +0200

    add age and geder as input

---
+++ frontend/src/common/components/Input.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Input = ({
+    label,
+    className,
+    isRequired,
+    onChange,
+    type,
+    selectOptions,
+    name,
+    value,
+    errorMessage,
+    selectFirst
+}) => {
+    return (
+        <div className={className + ' inline-flex'}>
+            {isRequired && <span className="required">*</span>}
+            {type === 'text' && (
+                <input
+                    autoComplete="off"
+                    onChange={onChange}
+                    required={isRequired}
+                    placeholder={label}
+                    name={name}
+                    type={type}
+                    value={value}
+                />
+            )}
+            {type === 'select' && (
+                <select
+                    onChange={onChange}
+                    name={name}
+                    className={!isRequired ? 'require-align' : ''}
+                >
+                    {selectFirst && (
+                        <option
+                            selected
+                            disabled
+                            hidden
+                            key="firstOption"
+                            defaultValue={'firstOption'}
+                        >
+                            Select...
+                        </option>
+                    )}
+                    {selectOptions &&
+                        selectOptions.map((item, index) => (
+                            <option key={'option' + index}>{item}</option>
+                        ))}
+                </select>
+            )}
+        </div>
+    );
+};
+
+// Input.propTypes = {
+//     label: PropTypes.string,
+//     className: PropTypes.string,
+//     onChange: PropTypes.func,
+//     isRequired: PropTypes.bool,
+//     type: PropTypes.string,
+//     selectOptions: PropTypes.array,
+//     name: PropTypes.string,
+//     value: PropTypes.object,
+//     touched: PropTypes.bool,
+//     selectFirst: PropTypes.bool
+// };
+
+export default Input;
--- frontend/src/components/rateMovie/UserInfoView.js
+++ frontend/src/components/rateMovie/UserInfoView.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
+import Input from 'common/components/Input';
 
 class UserInfoView extends Component {
-    handleChange = e => {
-        console.log(e.target.name);
+    handleChange = ({ target }) => {
         const { setReviewerDetails, reviewerDetails } = this.props;
         const details = { ...reviewerDetails };
-        details[e.target.name] = e.target.value;
+        details[target.name] = target.value;
         setReviewerDetails(details);
     };
 
-    handleSubmit = e => {
+    handleSubmit = event => {
         event.preventDefault();
         const { reviewerDetails } = this.props;
         console.log(reviewerDetails);
+    };
+
+    initAge = (start, end) => {
+        return Array.apply(null, {
+            length: end - start + 1
+        }).map((item, index) => start + index);
     };
 
     render() {
@@ -21,19 +27,25 @@ class UserInfoView extends Component {
             <form onSubmit={this.handleSubmit}>
                 <label>
                     age:
-                    <input
-                        type="text"
+                    <Input
+                        label="age"
+                        isRequired={true}
+                        type="select"
                         name="age"
                         value={reviewerDetails.age}
+                        selectOptions={this.initAge(18, 120)}
                         onChange={this.handleChange}
                     />
                 </label>
                 <label>
                     gender:
-                    <input
-                        type="text"
+                    <Input
+                        isRequired={true}
+                        type="select"
                         name="gender"
+                        selectOptions={['FEMALE', 'MALE', 'OTHER']}
                         value={reviewerDetails.gender}
+                        selectFirst={true}
                         onChange={this.handleChange}
                     />
                 </label>
