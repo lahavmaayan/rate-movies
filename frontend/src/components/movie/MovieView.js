ffe1a45ecc21cbfd4eac98ef7d0c3fa99b41b367
shani fedida <shani.fedida@gmail.com>
2019-01-14 14:34:57 +0200

    move the ratings mocks up

--- frontend/src/components/movie/MovieView.js
+++ frontend/src/components/movie/MovieView.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 import RatingsGrid from './RatingsGrid';
 
 class MovieView extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ratings: [
+                { feature: 'funny', rating: 4, maxRating: 5 },
+                { feature: 'interesting', rating: 3, maxRating: 5 },
+                { feature: 'female lead', rating: 3, maxRating: 5 },
+                { feature: 'happy ending', rating: 3, maxRating: 5 },
+                { feature: 'make you think', rating: 3, maxRating: 5 },
+                { feature: 'interesting2', rating: 3, maxRating: 5 }
+            ]
+        };
+    }
+
     render() {
         return (
             <div>
                 <div className="title">movie</div>
-                <RatingsGrid />
+                <RatingsGrid ratings={this.state.ratings} />
             </div>
         );
     }
