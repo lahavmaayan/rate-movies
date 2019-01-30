213ca2bb0dca95064d2fb5a519215af48b5db552
Iahel Ramos <iahel.ra@gigya-inc.com>
2019-01-30 23:59:44 +0200

    fix font awsome import

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import { createStore, compose, applyMiddleware } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
 import rootReducers from './services/rootReducers';
 import thunk from 'redux-thunk';
+import 'font-awesome/css/font-awesome.css';
 
 import App from './App';
 
