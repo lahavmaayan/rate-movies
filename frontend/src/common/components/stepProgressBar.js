fe638a056bec9b2b035f6a178cbc7a53b61484a9
Iahel Ramos <iahel.ra@gigya-inc.com>
2019-02-01 23:59:43 +0200

    more step progress bar css

--- frontend/src/common/components/stepProgressBar.js
+++ frontend/src/common/components/stepProgressBar.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import _ from 'lodash';
 const StepProgressBar = ({ stepsCount, currentStep, className }) => {
+    const getClassName = step => {
+        className = 'step fa ';
+        className +=
+            step < currentStep
+                ? 'fa-check-circle visited'
+                : step === currentStep
+                ? 'fa-circle active'
+                : 'fa-circle';
+        return className;
+    };
     return (
         <div className="step-progress">
             <div className="steps">
                 {_.range(1, stepsCount + 1).map(step => (
                     <div className="wrapper">
                         <div
-                            className={
-                                currentStep === step
-                                    ? 'step active fa fa-check-circle'
-                                    : 'step  fa fa-circle'
-                            }
+                            className={getClassName(step)}
                             key={step.toString()}
                         />
                         <div className="title">{'Step ' + step}</div>
