4cebef596d80adee90362583529f7998b44558ae
Yama <you@example.com>
2019-03-23 21:20:21 +0200

    created movieTile component and carousel component. Added to the server a new api 'getTopRatings' that returns a default JSON.

---
+++ frontend/src/components/movieTile/movieTile.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class MovieTile extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {hovered: false};
+    }
+
+    render() {
+        const { imageClass, imageHoveredClass } = this.props
+        return(
+            <div>
+                <a href={this.props.movieUrl}>
+                    <img width={140} height={209} alt="" src={this.props.imageUrl}
+                    onMouseOut={() => this.setState({hovered: false})}
+                    onMouseOver={() => this.setState({hovered: true})}
+                    className={this.state.hovered ? imageHoveredClass : imageClass}
+                    />
+                </a>
+            </div>
+        );
+    }
+}
+
+export default MovieTile;
