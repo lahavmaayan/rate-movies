5e508207fe4fc2485bcb7605d0e4c4cfdb34d667
Iahel Ramos <iahel.ra@gigya-inc.com>
2019-03-30 22:28:14 +0300

    add result grid + tile bar to home page

--- frontend/src/common/components/ResultsGrid.js
+++ frontend/src/common/components/ResultsGrid.js
@@ -1,28 +1,11 @@
 import React from "react";
 
-const tileData = [
-  {
-    img:
-      "https://m.media-amazon.com/images/M/MV5BMTE0YWFmOTMtYTU2ZS00ZTIxLWE3OTEtYTNiYzBkZjViZThiXkEyXkFqcGdeQXVyODMzMzQ4OTI@._V1_SY1000_CR0,0,675,1000_AL_.jpg",
-    title: "Titanic",
-    author: "IAhel ramos"
-  },
-  {
-    img:
-      "https://m.media-amazon.com/images/M/MV5BMTc5Nzc1OTk3OV5BMl5BanBnXkFtZTgwNDM1NTQ3NjM@._V1_UX182_CR0,0,182,268_AL_.jpg",
-    title: "Shalom",
-    author: "IAhel ramos1"
-  }
-];
 const ResultsGrid = props => {
-  const { classes } = props;
-
+  const { results } = props;
   return (
     <div className="result-grid">
-      {tileData.map(tile => (
-        <div key={tile.img}>
-          <img src={tile.img} />
-        </div>
+      {results.map(result => (
+        <div>{result}</div>
       ))}
     </div>
   );
--- frontend/src/components/movieTile/movieTile.js
+++ frontend/src/components/movieTile/movieTile.js
@@ -1,25 +1,30 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import GridTileBar from "./../../common/components/GridTileBar";
 class MovieTile extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {hovered: false};
-    }
+  constructor(props) {
+    super(props);
+    this.state = { hovered: false };
+  }
 
-    render() {
-        const { imageClass, imageHoveredClass } = this.props
-        return(
-            <div>
-                <a href={this.props.movieUrl}>
-                    <img width={140} height={209} alt="" src={this.props.imageUrl}
-                    onMouseOut={() => this.setState({hovered: false})}
-                    onMouseOver={() => this.setState({hovered: true})}
-                    className={this.state.hovered ? imageHoveredClass : imageClass}
-                    />
-                </a>
-            </div>
-        );
-    }
+  render() {
+    const { imageClass, imageHoveredClass, title, rating } = this.props;
+    return (
+      <div className="movie-tile">
+        <a href={this.props.movieUrl}>
+          <img
+            width={140}
+            height={209}
+            alt=""
+            src={this.props.imageUrl}
+            onMouseOut={() => this.setState({ hovered: false })}
+            onMouseOver={() => this.setState({ hovered: true })}
+            className={this.state.hovered ? imageHoveredClass : imageClass}
+          />
+        </a>
+        <GridTileBar title={title} data={"rating: " + rating} />
+      </div>
+    );
+  }
 }
 
 export default MovieTile;
