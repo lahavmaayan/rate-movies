b5d60c429be8455833b3816cf505cf7f40d0eb29
shani fedida <shani.fedida@gmail.com>
2019-04-11 21:18:22 +0300

    m

--- frontend/src/components/searchMovie/MultiSelectTags.js
+++ frontend/src/components/searchMovie/MultiSelectTags.js
@@ -3,15 +3,13 @@ import React, { Component } from 'react';
 class MultiSelectTags extends Component {
     render() {
         return (
-            <div className="btn-group" data-toggle="buttons">
-                <label className="btn btn-primary">
-                    <input type="checkbox" /> Checkbox 1 (pre-checked)
-                </label>
-                <label className="btn btn-primary">
-                    <input type="checkbox" /> Checkbox 2
-                </label>
-                <label className="btn btn-primary">
-                    <input type="checkbox" /> Checkbox 3
+            <div>
+                <input id="toggle" type="checkbox" className="hide-checkbox" />
+                <label
+                    htmlFor="toggle"
+                    className="badge badge-primary badge-pill capitalize movieTag"
+                >
+                    aaa
                 </label>
             </div>
         );
